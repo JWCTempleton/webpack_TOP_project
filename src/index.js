602b8af80613553998a237e8c60a7af51e7fde68
Jacob Templeton <jwctempleton@gmail.com>
2022-05-01 20:56:51 -0700

    Add webpack css loaders, add picture to dom element as proof of concept

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import "./style.css";
+
 console.log("WORKING!");
+console.log("watch is working too");
+
+const picture = document.createElement("img");
+picture.src = "../dist/restaurant.jpg";
+picture.classList.add(".header__img");
+
+const content = document.querySelector("#content");
+content.appendChild(picture);
